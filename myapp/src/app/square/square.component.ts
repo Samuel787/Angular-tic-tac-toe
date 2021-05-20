14b0a8d84d65aec2a2bd7b6080c1d7c8a9a4f4c4
Samuel787 <suther.david.samuel@gmail.com>
2021-05-21 01:12:38 +0800

    Disable continuation of game after winning

--- myapp/src/app/square/square.component.ts
+++ myapp/src/app/square/square.component.ts
@@ -3,9 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button style="height:200px; width:200px;" nbButton hero status="danger" *ngIf="!value">{{ value }}</button>
-      <button style="height:200px; width:200px;" nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-      <button style="height:200px; width:200px;" nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+      <button style="height:200px; width:200px; font-size: 60px;" nbButton hero status="danger" *ngIf="!value">{{ value }}</button>
+      <button style="height:200px; width:200px; font-size: 60px;" nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+      <button style="height:200px; width:200px; font-size: 60px;" nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
   styles: ['']
 })
